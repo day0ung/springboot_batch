20c121018a680f18ea4471e9859d95187bdc00d8
day0ung <jdy9436@naver.com>
2023-01-08 18:38:34 +0900

    -batch 관련세팅

--- batch/src/main/java/com/boot/batch/BatchApplication.java
+++ batch/src/main/java/com/boot/batch/BatchApplication.java
@@ -3,13 +3,15 @@ package com.boot.batch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableBatchProcessing //배치 활성화
 @SpringBootApplication
 public class BatchApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BatchApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(BatchApplication.class, args);
+        //JobLauncher.run(ctx, args);
     }
 
 }
